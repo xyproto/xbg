f00cd9523d7e336e84f857fa8e001e666cb3fa0e
Alexander F. Rødseth <rodseth@gmail.com>
2019-11-18 11:50:18 +0100

    Simplify the example

--- cmd/grumpybg/main.go
+++ cmd/grumpybg/main.go
@@ -18,43 +18,31 @@ func exists(path string) bool {
 // SetWallpaper will set the given image filename as the wallpaper,
 // regardless of which display server, window manager or desktop environment is in use.
 func SetWallpaper(imageFilename, mode string, verbose bool) error {
+	// This check is redundant, but it's nice to check it before checking if the WM is ready
 	if !exists(imageFilename) {
 		return fmt.Errorf("no such file: %s", imageFilename)
 	}
-	var lastErr error
 	wm := xbg.X11{}
-	if wm.Running() && wm.ExecutablesExists() {
+	if !(wm.Running() && wm.ExecutablesExists()) {
+		return errors.New("found no working method for setting the desktop wallpaper, maybe X11 is not installed or DISPLAY not set")
+	}
+	if verbose {
+		fmt.Printf("Using the %s backend.\n", wm.Name())
+	}
+	wm.SetVerbose(verbose)
+	wm.SetMode(mode)
+	if err := wm.SetWallpaper(imageFilename); err != nil {
 		if verbose {
-			fmt.Printf("Using the %s backend.\n", wm.Name())
+			fmt.Fprintf(os.Stderr, "failed: %v\n", err)
 		}
-		wm.SetVerbose(verbose)
-		wm.SetMode(mode)
-		if err := wm.SetWallpaper(imageFilename); err != nil {
-			lastErr = err
-			switch wm.Name() {
-			case "Weston":
-				// If the current windowmanager is Weston, no method is currently available
-				return err
-			default:
-				if verbose {
-					fmt.Fprintf(os.Stderr, "failed: %v\n", err)
-				}
-				// If the wallpaper mode is wrong, don't try the next backend, but return the error
-				if strings.Contains(err.Error(), "invalid desktop wallpaper mode") {
-					return err
-				}
-				// This did not work out
-				return errors.New("could not use the X11 backend")
-			}
-		} else {
-			return nil
+		// If the wallpaper mode is wrong, don't try the next backend, but return the error
+		if strings.Contains(err.Error(), "invalid desktop wallpaper mode") {
+			return err
 		}
+		// This did not work out
+		return errors.New("could not use the X11 backend")
 	}
-	if lastErr != nil {
-		return fmt.Errorf("found no working method for setting the desktop wallpaper:\n%v", lastErr)
-	}
-	return errors.New("found no working method for setting the desktop wallpaper")
-
+	return nil
 }
 
 func main() {
